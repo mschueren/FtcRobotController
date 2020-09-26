f0f2761f863fced2f0926a6eeff3f518aa1f4f01
Mike Schueren <mschueren@me.com>
2020-09-26 14:05:43 -0500

    Updated ExampleCode

---
+++ ExampleCode/src/main/java/org/firstinspires/ftc/examplecode/Driving/ExampleDriveByGyro.java
@@ -0,0 +1,148 @@
+/* Copyright (c) 2015 Qualcomm Technologies Inc
+
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without modification,
+are permitted (subject to the limitations in the disclaimer below) provided that
+the following conditions are met:
+
+Redistributions of source code must retain the above copyright notice, this list
+of conditions and the following disclaimer.
+
+Redistributions in binary form must reproduce the above copyright notice, this
+list of conditions and the following disclaimer in the documentation and/or
+other materials provided with the distribution.
+
+Neither the name of Qualcomm Technologies Inc nor the names of its contributors
+may be used to endorse or promote products derived from this software without
+specific prior written permission.
+
+NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS
+LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+"AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
+FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE. */
+
+package org.firstinspires.ftc.examplecode.Driving;
+
+import com.qualcomm.hardware.bosch.BNO055IMU;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
+import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
+
+import java.util.Locale;
+
+@TeleOp(name = "Example Drive by Gyro", group = "DriveExample")
+@Disabled
+
+public class ExampleDriveByGyro extends OpMode {
+
+    DcMotor rightMotor;
+    DcMotor leftMotor;
+    BNO055IMU imu;
+
+    Orientation angles;
+    double heading;
+    int rotations = 0;
+    int targetDirection = 0;
+    double directionCorrectSpd = 0;
+
+    @Override
+    public void init() {
+        rightMotor = hardwareMap.dcMotor.get("right motor");
+        leftMotor = hardwareMap.dcMotor.get("left motor");
+
+        rightMotor.setDirection(DcMotor.Direction.REVERSE);
+
+        BNO055IMU.Parameters parameters = new BNO055IMU.Parameters();
+        parameters.angleUnit = BNO055IMU.AngleUnit.DEGREES;
+        imu = hardwareMap.get(BNO055IMU.class, "imu");
+        imu.initialize(parameters);
+
+        telemetry.addData("Status:", "Robot is Initialized");
+    }
+
+    @Override
+    public void init_loop() {
+        angles = imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, AngleUnit.DEGREES);
+
+        telemetry.addData("1", "Integrated Heading: " + getIntegratedHeading());
+        telemetry.addData("2", "heading: " + angles.firstAngle);
+        telemetry.addData("1 Right Motor Pos", rightMotor.getCurrentPosition());
+        telemetry.addData("2 Left Motor Pos", leftMotor.getCurrentPosition());
+    }
+
+
+    @Override
+    public void start() {
+        rightMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        leftMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+    }
+
+
+    @Override
+    public void loop() {
+        angles = imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, AngleUnit.DEGREES);
+
+        if(gamepad1.dpad_left){
+            targetDirection++;
+        } else if(gamepad1.dpad_right){
+            targetDirection--;
+        }
+
+        if(gamepad1.a) {
+            directionCorrectSpd = (targetDirection - getIntegratedHeading())/100;
+            rightMotor.setPower(.3 + directionCorrectSpd);
+            leftMotor.setPower(.3 - directionCorrectSpd);
+        }
+        else {
+            rightMotor.setPower(0);
+            leftMotor.setPower(0);
+        }
+
+        // send the info back to driver station using telemetry function.
+        telemetry.addData("1 Right Motor Power", rightMotor.getCurrentPosition());
+        telemetry.addData("2 Left Motor Power", leftMotor.getCurrentPosition());
+        telemetry.addData("3", "Integrated Heading: " + getIntegratedHeading());
+        telemetry.addData("4", "heading: " + formatAngle(angles.angleUnit, angles.firstAngle));
+    }
+
+
+    @Override
+    public void stop() {
+        rightMotor.setPower(0);
+        leftMotor.setPower(0);
+    }
+
+    String formatAngle(AngleUnit angleUnit, double angle) {
+        return formatDegrees(AngleUnit.DEGREES.fromUnit(angleUnit, angle));
+    }
+
+    String formatDegrees(double degrees){
+        return String.format(Locale.getDefault(), "%.1f", AngleUnit.DEGREES.normalize(degrees));
+    }
+
+    public double getIntegratedHeading() {
+        if(heading - (rotations * 360 + angles.firstAngle) > 200) {
+            rotations++;
+        }
+        else if(heading - (rotations * 360 + angles.firstAngle) < -200) {
+            rotations--;
+        }
+
+        heading = rotations * 360 + angles.firstAngle;
+        return heading;
+    }
+}
---
+++ ExampleCode/src/main/java/org/firstinspires/ftc/examplecode/ExampleMechanumDrive.java
@@ -0,0 +1,176 @@
+package org.firstinspires.ftc.examplecode;
+
+import com.qualcomm.hardware.bosch.BNO055IMU;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.ElapsedTime;
+import com.qualcomm.robotcore.util.Range;
+
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
+import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
+
+
+@TeleOp(name = "Mechanum Drive", group = "Example")
+//@Disabled
+
+public class ExampleMechanumDrive extends OpMode {
+
+    DcMotor frMotor, flMotor, brMotor, blMotor;
+    BNO055IMU imu;
+    Orientation gyroAngles;
+
+    double frPower=0, flPower=0, brPower=0, blPower=0;
+    double maxMotorPower=0;
+    double driveSpeed = 0;              //The speed the robot should move
+    double driveRotation = 0;           //Causes the robot to rotate
+    double x, y, joystickAngle, joystickAngle360;
+    double desiredRobotHeading;
+    int rotations = 0;
+
+    @Override
+    public void init() {
+        frMotor = hardwareMap.dcMotor.get("frontright");
+        flMotor = hardwareMap.dcMotor.get("frontleft");
+        brMotor = hardwareMap.dcMotor.get("backright");
+        blMotor = hardwareMap.dcMotor.get("backleft");
+
+        frMotor.setDirection(DcMotor.Direction.REVERSE);
+        brMotor.setDirection(DcMotor.Direction.REVERSE);
+
+        BNO055IMU.Parameters parameters = new BNO055IMU.Parameters();
+        parameters.angleUnit = BNO055IMU.AngleUnit.DEGREES;
+        imu = hardwareMap.get(BNO055IMU.class, "imu");
+        imu.initialize(parameters);
+
+        telemetry.addData("Status:", "Robot is Initialized");
+    }
+
+
+    @Override
+    public void init_loop() {
+        gyroAngles = imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, AngleUnit.DEGREES);
+
+        telemetry.addData("1", "Integrated Heading: " + getIntegratedHeading());
+        telemetry.addData("2", "heading: " + gyroAngles.firstAngle);
+        telemetry.addData("1 Right Motor Pos", frMotor.getCurrentPosition());
+        telemetry.addData("2 Left Motor Pos", flMotor.getCurrentPosition());
+    }
+
+
+    @Override
+    public void start() { }
+
+
+    @Override
+    public void loop() {
+        //Get gyro heading
+        gyroAngles = imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, AngleUnit.DEGREES);
+
+        //Positive values for x axis are joystick right
+        //Positive values for y axis are joystick down
+        y = Range.clip(-gamepad1.right_stick_y,-1,1);
+        x = Range.clip(gamepad1.right_stick_x,-1,1);
+        joystickAngle = Math.atan2(-x,y);
+        joystickAngle360 = joystickAngle >= 0 ? joystickAngle : (2*Math.PI) + joystickAngle;
+
+        //finding the hypotenuse to calculate drive speed
+        driveSpeed = Range.clip(Math.sqrt(y * y + x * x), -1, 1);
+
+        //Determine if the operator wants the robot to rotate and if so in what direction.
+        //If the robot is rotated and is not supposed to have rotated, it will return to the robotHeading
+        if (gamepad1.right_trigger > .05 || gamepad1.left_trigger > .05) {
+            driveRotation = gamepad1.left_trigger - gamepad1.right_trigger;
+            desiredRobotHeading = getIntegratedHeading();
+        } else if (Math.abs(desiredRobotHeading - getIntegratedHeading() ) > 5){
+            driveRotation = (desiredRobotHeading - getIntegratedHeading()) / (Math.abs(desiredRobotHeading - getIntegratedHeading()))  * .5;
+        } else {
+            driveRotation = 0;
+        }
+
+        //Calculate the power for each motor to translate in the desired direction
+        flPower = Math.cos(joystickAngle360 - Math.toRadians(desiredRobotHeading) + Math.PI / 4);
+        frPower = Math.sin(joystickAngle360 - Math.toRadians(desiredRobotHeading) + Math.PI / 4);
+        blPower = Math.sin(joystickAngle360 - Math.toRadians(desiredRobotHeading) + Math.PI / 4);
+        brPower = Math.cos(joystickAngle360 - Math.toRadians(desiredRobotHeading) + Math.PI / 4);
+
+        //Determine the maximum calulated motor power
+        maxMotorPower = Math.max(Math.max(Math.max(Math.abs(flPower), Math.abs(frPower)), Math.abs(blPower)), Math.abs(brPower));
+
+        //Ratio the powers for disrection
+        flPower = flPower / maxMotorPower;
+        frPower = frPower / maxMotorPower;
+        blPower = blPower / maxMotorPower;
+        brPower = brPower / maxMotorPower;
+
+        //Calculate the power for each motor to go the direction, speed and rotation gathered above
+        flPower = driveSpeed * flPower - driveRotation;
+        frPower = driveSpeed * frPower + driveRotation;
+        blPower = driveSpeed * blPower - driveRotation;
+        brPower = driveSpeed * brPower + driveRotation;
+
+        //Determine the maximum calulated motor power
+        maxMotorPower = Math.max(Math.max(Math.max(Math.abs(flPower), Math.abs(frPower)), Math.abs(blPower)), Math.abs(brPower));
+
+        //If any motor power is not in the range {-1, 1}, scale all motors to fit in the range and still move as directed
+        if (Math.abs(maxMotorPower) > 1) {
+            flPower = flPower / maxMotorPower;
+            frPower = frPower / maxMotorPower;
+            blPower = blPower / maxMotorPower;
+            brPower = brPower / maxMotorPower;
+        } else if(Math.abs(maxMotorPower) < .03) {
+            flPower = 0;
+            frPower = 0;
+            blPower = 0;
+            brPower = 0;
+        }
+
+        //Set the actual motor powers
+        flMotor.setPower(flPower);
+        frMotor.setPower(frPower);
+        blMotor.setPower(blPower);
+        brMotor.setPower(brPower);
+
+        // send the info back to driver station using telemetry function.
+        telemetry.addData("Left front", flPower);
+        telemetry.addData("Right front", frPower);
+        telemetry.addData("Left back", blPower);
+        telemetry.addData("Right back", brPower);
+        telemetry.addData("Drive Rotation", driveRotation);
+        telemetry.addData("d_Robot Heading", desiredRobotHeading);
+        telemetry.addData("Gyro", gyroAngles);
+        telemetry.addData("Gyro Int ZTot", getIntegratedHeading());
+        telemetry.addData("Gyro Rotations", rotations);
+        telemetry.addData("Drive Speed", driveSpeed);
+        telemetry.addData("x", x);
+        telemetry.addData("y", y);
+        telemetry.addData("Joystick Angle", Math.toDegrees(joystickAngle));
+        telemetry.addData("Joystock Angle360", Math.toDegrees(joystickAngle360));
+    }
+
+
+    @Override
+    public void stop() {
+        flMotor.setPower(0);
+        frMotor.setPower(0);
+        blMotor.setPower(0);
+        brMotor.setPower(0);
+    }
+
+
+    private double getIntegratedHeading() {
+        if(desiredRobotHeading - (rotations * 360 + gyroAngles.firstAngle) > 200) {
+            rotations++;
+        }
+        else if(desiredRobotHeading - (rotations * 360 + gyroAngles.firstAngle) < -200) {
+            rotations--;
+        }
+
+        return (rotations * 360 + gyroAngles.firstAngle);
+    }
+
+}
